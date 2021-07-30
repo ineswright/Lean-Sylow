a083465bd139e76b03e80bd27ebec18ad2ce4000
Ines W <ineswright7@gmail.com>
2021-07-30 17:58:45 +0100

    with erics working solution

--- src/test.lean
+++ src/test.lean
@@ -1,11 +1,20 @@
 import data.nat.prime
 import number_theory.padics.padic_norm
+import data.pnat.factors
 
+
+@[simp]
+lemma prime_multiset.coe_add (m n : prime_multiset) : ↑(m + n) = (m + n : multiset ℕ) :=
+is_add_monoid_hom.map_add _ _ _
+
+attribute [simp] pnat.coe_nat_factor_multiset
 
 lemma factors_mul {p q : ℕ} (hp : 0 < p) (hq : 0 < q) : (p * q).factors ~ p.factors ++ q.factors :=
 begin
-
-  sorry,
+  rw ←multiset.coe_eq_coe,
+  have := pnat.factor_multiset_mul ⟨p, hp⟩ ⟨q, hq⟩,
+  rw ←prime_multiset.coe_nat_injective.eq_iff at this,
+  simpa using this,
 end
 
 
@@ -14,8 +23,7 @@ begin
   haveI : fact p.prime := ⟨hprime⟩,
   induction y,
   { simp [pow_zero, nat.factors_one] },
-  { 
-    apply list.repeat_perm.1 (list.perm.trans _ (factors_mul (nat.prime.pos hprime) (pow_pos (nat.prime.pos hprime) y_n)).symm),
+  { apply list.repeat_perm.1 (list.perm.trans _ (factors_mul (nat.prime.pos hprime) (pow_pos (nat.prime.pos hprime) y_n)).symm),
     simp [nat.factors_prime hprime, y_ih] }
 end
 
